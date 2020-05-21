f639de984f70710d7a4e75857c3b5038bcd9ab64
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-21 19:53:50 -0400

    modified config to use dotenv

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+const PORT = process.env.PORT || 3001;
+var db = require("./models"); //grabs database models
 
 app.use([
   express.urlencoded({ extended: true }),
@@ -18,6 +21,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log("🚀  API server now on port", PORT);
+
+db.sequelize.sync( { force: true } ).then(function() { //syncs our models to database, remove 'force: true' in production so we dont destroy our data
+  app.listen(PORT, function() {
+    console.log("🚀  API server now on port", PORT);
+  });
 });
