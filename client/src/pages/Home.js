7d2c069c68642025b150fe215b5ab532ee8cd878
Ryan Kaplan <ryankaplan7@gmail.com>
2020-05-21 18:19:17 -0400

    added initial components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+function Home() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-12">
+
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Home;
