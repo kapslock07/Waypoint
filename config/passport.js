73347b413db58fbb9b934bd683d86f3c10a64bf9
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-21 19:55:59 -0400

    removed config.json, passing it to nicholas to add, installed oauth and google oauth

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 
 var db = require("../models");
 
@@ -44,6 +45,21 @@ passport.serializeUser(function (user, cb) {
 passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
 });
+
+
+// passport.use('provider', new OAuth2Strategy({
+//     authorizationURL: 'https://www.provider.com/oauth2/authorize',
+//     tokenURL: 'https://www.provider.com/oauth2/token',
+//     clientID: '123-456-789',
+//     clientSecret: 'shhh-its-a-secret',
+//     callbackURL: 'https://www.example.com/auth/provider/callback'
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     User.findOrCreate(..., function(err, user) {
+//       done(err, user);
+//     });
+//   }
+// ));
 
 // Exporting our configured passport
 module.exports = passport;
