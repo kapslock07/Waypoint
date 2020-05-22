0ccb5dd1dfac27cbd6a6afd667716074942ff12e
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-21 20:05:47 -0400

    created base for game model

---
+++ models/games.js
@@ -0,0 +1,30 @@
+// Creating our Games model
+module.exports = function(sequelize, DataTypes) {
+  var Games = sequelize.define("Games", {
+    
+    //Id of game
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    //Game Title
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    //Game Description
+    desc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "No Description Available"
+    },
+    //image for game
+    image: {
+        allowNull: true,
+        defaultValue: "none"
+    }
+  });
+
+  return Games;
+};
