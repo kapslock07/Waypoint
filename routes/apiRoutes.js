c5f01b530cbc725b7e54686edfb776db1b41fd88
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-21 20:32:36 -0400

    began adding api routes, added games route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,32 @@
 
 module.exports = (server, db) => {
 
+    /*      * Here are the basic api routes for games *
+
+            -GET /api/games -> used to search for games
+            -GET /api/users -> used to get user info
+
+
+    */
+
+    server.get("/api/games", (req, res) => { //should use authentication middleware here
+        let query = req.body.query;
+
+        db.Games.findAll({
+            where: {
+                title: {
+                    [Op.like]: '%' + query + '%'
+                }
+            },
+            include: [db.User]
+        }).then(data => {
+            res.json(data);
+        })
+    });
+
+    server.get("/api/users", (req,res) => {
+
+    });
+    
+
 }
