f9098c9fee795b56736e4978b70b4171b8b77e34
Ryan Kaplan <ryankaplan7@gmail.com>
2020-05-22 20:06:04 -0400

    added icons and react bootstrap elements to navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Form } from 'react-bootstrap';
 import SampleImg from "../../assets/images/samplelogo.jpg";
 
 import "./style.css";
@@ -15,9 +15,9 @@ function Nav() {
             <Navbar.Brand href="#home">
                 <img
                     src={SampleImg}
-                    width="75"
-                    height="75"
-                    className="d-inline-block align-top"
+                    width="100"
+                    height="100"
+                    className="d-inline-block align-top ml-4"
                     alt="React Bootstrap logo"
                 />
             </Navbar.Brand>
@@ -33,13 +33,18 @@ function Nav() {
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <form className="form-inline my-2 my-lg-0 mx-auto">
-                    <input id="dbSearchInput" className="form-control mr-sm-2" type="search" placeholder="Search For A Game"
+                <Form.Group className="form-inline my-2 my-lg-0 mx-auto">
+                    <Form.Control size="lg" type="text" placeholder="Search For A Game" className="gameSearchInput form-control mr-sm-2" />
+                    <button className="gameSearchButton btn btn-outline-light my-2 my-sm-0"
+                        type="submit">Search</button>
+                </Form.Group>
+                {/* <form className="form-inline my-2 my-lg-0 mx-auto">
+                    <input className="gameSearchInput form-control mr-sm-2" type="search" placeholder="Search For A Game"
                         aria-label="Search">
                     </input>
-                    <button id="dbSearchButton" class="btn btn-outline-secondary my-2 my-sm-0"
+                    <button className="gameSearchButton btn btn-outline-light my-2 my-sm-0"
                         type="submit">Search</button>
-                </form>
+                </form> */}
 
                 <div className="navbar-nav">
                     <Link
@@ -48,15 +53,22 @@ function Nav() {
                             location.pathname === "/" ? "nav-link active" : "nav-link"
                         }
                     >
-                        Home
+                        <div className="mx-4">
+                            <i className="fa fa-3x fa-gamepad d-flex justify-content-center"></i>
+                            <h5>Home</h5>
+                        </div>
                     </Link>
                     <Link
                         to="/saved"
                         className={
                             location.pathname === "/chats" ? "nav-link active" : "nav-link"
                         }
+
                     >
-                        Chat
+                        <div className="mx-4">
+                            <i className="fa fa-3x fa-comments d-flex justify-content-center"></i>
+                            <h5>Chat</h5>
+                        </div>
                     </Link>
                 </div>
             </div>
