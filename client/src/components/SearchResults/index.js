89566db77c598060910cb7e1510de32fd99979d5
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-29 17:41:31 -0400

    added functionality to iterate through users

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -8,14 +8,19 @@ import API from "../../utils/API";
 
 function SearchResults() {
 
-    const [users, setUsers] = React.useState([]);
+    let [users, setUsers] = React.useState([]); //state for users
 
-    React.useEffect(() => {
-        API.getUsers().then(data => {
-            setUsers(data);
-            console.log(users);
-        });
-    });
+    React.useEffect(() => { //grabs users
+        loadUsers();
+    }, []);
+
+
+    function loadUsers(){
+        API.getUsers().then(res => {
+            setUsers(res.data);
+        })
+        .catch(err => console.log(err));
+    }
 
     return (
         <Container fluid>
@@ -25,12 +30,10 @@ function SearchResults() {
                         <Header name={"Users"} />
                     </MDBAnimation>
                     <MDBAnimation type="fadeInUp">
-                        <SearchResultsBox username={"GoofyName234"} image={"https://image.flaticon.com/icons/svg/1880/1880988.svg"} favoriteGames={["Final Fantasy 80", "Uniracers", "Duck Hunt"]} favoriteConsoles={["Xbox 360", "Nintendo Switch"]} />
-                        <SearchResultsBox username={"IamMe33"} image={"https://www.nawrb.com/wp-content/uploads/2016/04/user-307993_960_720.png"} favoriteGames={["Donkey Kong Country", "Mario Kart", "Zelda"]} favoriteConsoles={["PC", "Nintendo Switch"]} />
-                        <SearchResultsBox username={"JimBob22"} image={"https://www.kodefork.com/static/users/images/user.png"} favoriteGames={["Rocket League", "Madden 20", "Fortnite"]} favoriteConsoles={["Playstation 4", "PC"]} />
-                        <SearchResultsBox username={"GoofyName234"} image={"https://image.flaticon.com/icons/svg/1880/1880988.svg"} favoriteGames={["Final Fantasy 80", "Uniracers", "Duck Hunt"]} favoriteConsoles={["Xbox 360", "Nintendo Switch"]} />
-                        <SearchResultsBox username={"IamMe33"} image={"https://www.nawrb.com/wp-content/uploads/2016/04/user-307993_960_720.png"} favoriteGames={["Donkey Kong Country", "Mario Kart", "Zelda"]} favoriteConsoles={["PC", "Nintendo Switch"]} />
-                        <SearchResultsBox username={"JimBob22"} image={"https://www.kodefork.com/static/users/images/user.png"} favoriteGames={["Rocket League", "Madden 20", "Fortnite"]} favoriteConsoles={["Playstation 4", "PC"]} />
+
+                        { users.length !== 0 ? users.map(e => {
+                            return  <SearchResultsBox key={e.id} username={e.userName} image={"https://image.flaticon.com/icons/svg/1880/1880988.svg"} favoriteGames={["Final Fantasy 80", "Uniracers", "Duck Hunt"]} favoriteConsoles={["Xbox 360", "Nintendo Switch"]} />
+                        }) : <h1>No Users Found</h1> }
                     </MDBAnimation>
                 </Col>
             </Row>
