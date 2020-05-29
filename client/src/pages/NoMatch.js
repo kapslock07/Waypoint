e970602d5944bca8f930ef15740002e11633e58f
Ryan Kaplan <ryankaplan7@gmail.com>
2020-05-28 21:04:22 -0400

    fixed top padding on body

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -7,12 +7,14 @@ function NoMatch() {
     return (
         <Container fluid>
             <Nav />
-            <Row>
-                <Col xs={12} className="text-center">
-                    <h1>404 Page Not Found</h1>
-                    <img className="my-5" src="https://www.digilyfe.co/wp-content/uploads/2018/06/90a309bfe92aebf0add9b46d94f8f4d0-aliens-movie.jpg" alt="NoMatch" ></img>
-                </Col>
-            </Row>
+            <div className="noMatchComponent">
+                <Row>
+                    <Col xs={12} className="text-center">
+                        <h1>404 Page Not Found</h1>
+                        <img className="my-5" src="https://www.digilyfe.co/wp-content/uploads/2018/06/90a309bfe92aebf0add9b46d94f8f4d0-aliens-movie.jpg" alt="NoMatch" ></img>
+                    </Col>
+                </Row>
+            </div>
         </Container>
     );
 }
