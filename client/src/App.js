d91218e12de1d452e68c8e0ef80f2e1a8e715043
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-30 13:22:22 -0400

    adjusted response for authentication to be only id for user, fixed the authentication redirectt, added placeholder for where loading component should go

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import AuthRoute from "./components/AuthRoute";
 import "./App.css";
 
