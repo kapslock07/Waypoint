0a283b11b6c02c97ea7d56b61a50e7adffa5ccd0
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-04 15:47:38 -0400

    changed href and ports

--- config/messageServer.js
+++ config/messageServer.js
@@ -5,29 +5,32 @@ module.exports = (app) => {
     const chatServer = require('http').createServer(app);
     let io = require('socket.io')(chatServer);
     const chatController = require("../controllers/chatController");
+    require(dotenv).config();
+
+    const PORT = process.env.PORT || 3002;
 
     let connectedUsers = new Map(); //this is where we store connected users
 
-    
+
     io.on('connection', (socket) => {
 
         socket.on("user_connect", (data) => { //when a client connects to the server
-            
+
             connectedUsers.set(data, {
                 sId: socket.id,
                 uId: data
             });
 
-           // console.log("Total Users Online", connectedUsers.size);
+            // console.log("Total Users Online", connectedUsers.size);
             socket.emit("user_connect", connectedUsers.get(data).sId);
         });
 
         socket.on("created_chat", (data) => { //when a client creates a chat with another user
             //get client who will recieve chat notification!
             let reciever = connectedUsers.get(data.joineeId);
-            
-            if(reciever === undefined){
-               // console.log("One user not online") this is useless
+
+            if (reciever === undefined) {
+                // console.log("One user not online") this is useless
             }
             else {
                 io.to(reciever.sId).emit("created_chat", { //have this feature either send if online, or instead just add chat notification in db?
@@ -44,18 +47,18 @@ module.exports = (app) => {
             let reciever = data.reciever;
             let recSocket = connectedUsers.get(reciever);
 
-            let chatData = { 
+            let chatData = {
                 authorId: sender,
                 message: message,
                 ChatId: chatId
             }
 
-            if(recSocket === undefined){
+            if (recSocket === undefined) {
                 chatController.createMessage(chatData).then(createdMessage => {
                     socket.emit("call_send_message");
                 });
             }
-            else{
+            else {
                 socket.emit("call_send_message");
 
                 chatController.createMessage(chatData).then(createdMessage => {
@@ -66,14 +69,14 @@ module.exports = (app) => {
 
         socket.on("disconnect", () => {
             connectedUsers.forEach(e => {
-                if(e.sId == socket.id){
-                   connectedUsers.delete(e.uId);
+                if (e.sId == socket.id) {
+                    connectedUsers.delete(e.uId);
                 }
             });
         });
     });
 
-    chatServer.listen(3002, () => {
+    chatServer.listen(PORT, () => {
         console.log("Chat server started 🖥");
     });
 }
