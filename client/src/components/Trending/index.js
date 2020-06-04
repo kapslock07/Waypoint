c213cddbeadd0a9db3b167032f24bad6013abd92
Ryan Kaplan <ryankaplan7@gmail.com>
2020-06-04 12:28:17 -0400

    updated trending games to those that exist on our site

--- client/src/components/Trending/index.js
+++ client/src/components/Trending/index.js
@@ -9,16 +9,16 @@ function Trending() {
                 <Col xs={12}>
                     <div className="trending text-center p-4 z-depth-2">
                         <h3 className="mb-4"><strong>Trending Games</strong></h3>
-                        <img src="https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg" alt="trending1" width="100" height="100"></img>
-                        <p>Animal Crossing</p>
-                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP9000-CUSA17357_00-MLBTHESHOW20STND/1590452727000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending2" width="100" height="100"></img>
-                        <p>MLB The Show 20</p>
-                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP9000-CUSA07820_00-THELASTOFUS2STND/1590453969000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending3" width="100" height="100"></img>
-                        <p>The Last of Us Part II</p>
-                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0002-CUSA08829_00-CODMWTHEGAME0001/1590454136000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending3" width="100" height="100"></img>
-                        <p>Call of Duty: Modern Warfare</p>
-                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP1001-CUSA16310_00-NBA2K20GOLD00000/1590455129000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending3" width="100" height="100"></img>
-                        <p>NBA 2K20</p>
+                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP1477-CUSA07022_00-FORTNITETESTING1/1591232660000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending1" width="100" height="100"></img>
+                        <p>Fortnite</p>
+                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0002-CUSA01842_00-OWLEGENDARY00000/1591232986000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending2" width="100" height="100"></img>
+                        <p>Overwatch</p>
+                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP2002-CUSA01163_00-ROCKETLEAGUENA01/1591232812000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending3" width="100" height="100"></img>
+                        <p>Rocket League</p>
+                        <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0002-CUSA08829_00-CODWARZONE000001/1591232215000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000" alt="trending3" width="100" height="100"></img>
+                        <p>Call of Duty: Warzone</p>
+                        <img src="https://www.therendezvous.rocks/wp-content/uploads/2015/06/LolPic.jpg" alt="trending3" width="100" height="100"></img>
+                        <p>League of Legends</p>
                     </div>
                 </Col>
             </Row>
