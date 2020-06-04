5f76d87a6ee5b6f3e07cd17a0a3cf09f2fb1242a
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-03 23:53:05 -0400

    successfully merged and fixed display of items

--- client/src/components/SearchResultsBox/index.js
+++ client/src/components/SearchResultsBox/index.js
@@ -4,11 +4,7 @@ import ChatBtn from "../ChatBtn";
 import "./style.css";
 
 function searchResultsBox(props) {
-
-
-
     return (
-<<<<<<< HEAD
         <li id={props.id}>
             <Container fluid>
                 <Row>
@@ -25,7 +21,7 @@ function searchResultsBox(props) {
                                 <Col lg={4}>
                                     <h5><strong>Favorite Consoles</strong></h5>
 
-                                    {!props.favoriteConsoles ? Array.isArray(props.favoriteConsoles) ? props.favoriteConsoles.map((console, i) => (
+                                    {props.favoriteConsoles ? Array.isArray(props.favoriteConsoles) ? props.favoriteConsoles.map((console, i) => (
                                         <p>{console}</p>
                                     )) : <p>props.favoriteConsoles</p>
                                         : <p>No favorite platforms!</p>
@@ -34,7 +30,7 @@ function searchResultsBox(props) {
                                 <Col lg={4}>
                                     <h5><strong>Favorite Games</strong></h5>
 
-                                    {!props.favoriteGames ? Array.isArray(props.favoriteGames) ? props.favoriteGames.map((game, i) => (
+                                    {props.favoriteGames ? Array.isArray(props.favoriteGames) ? props.favoriteGames.map((game, i) => (
                                         <p>{game}</p>
                                     )) : <p>props.favoriteGames</p> : <p>No favorite games!</p>}
                                 </Col>
@@ -46,41 +42,6 @@ function searchResultsBox(props) {
 
             </Container >
         </li>
-=======
-        <Container fluid>
-            <Row>
-                <Col lg={12}>
-                    <div className="searchResultsBox z-depth-2 p-2 mb-3">
-                        <Row>
-                            <Col lg={4} className="text-center">
-                                <div>
-                                    <h5>{props.username}</h5>
-                                    <img src={props.image} alt="User Image" width="25%"></img>
-                                </div>
-                                <ChatBtn joineeId={props.id} />
-                            </Col>
-                            <Col lg={4}>
-                                <h5><strong>Favorite Consoles</strong></h5>
-
-                                {props.favoriteConsoles.map((console, i) => (
-                                    <p key={console}>{console}</p>
-                                ))}
-                            </Col>
-                            <Col lg={4}>
-                                <h5><strong>Favorite Games</strong></h5>
-
-                                {props.favoriteGames.map((game, i) => (
-                                    <p key={game}>{game}</p>
-                                ))}
-                            </Col>
-
-                        </Row>
-                    </div>
-                </Col>
-            </Row>
-
-        </Container >
->>>>>>> 216b27b0245c405148e4d6b16019f4cad7c4cdf3
     );
 }
 
